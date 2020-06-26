312b2b80b2f6e3fde9740e6a2ce95861724b6573
yang <59075027+yangyu1054@users.noreply.github.com>
2020-06-25 23:51:14 -0700

    remove test_pc.sh

--- test_pc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ../bin/activate
-python --version
-python infer_EXR.py --image_dir ../data/train_data/training_10_images/10_HDR_EXR_PQ --output_dir /Users/yuyang/571_sub/data/test_data/results
-
-
